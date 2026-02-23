dd6e150da0112666f8073e783d7ad3c6311cb59f
pelmennka100208 <vladzo16092006@gmail.com>
2026-02-23 09:34:42 +0200

    сcreate program

--- KG_LAB1/Form1.Designer.cs
+++ KG_LAB1/Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,182 +15,195 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
             label1 = new Label();
             xCoordinateTextBox = new TextBox();
-            yCoordinateTextBox = new TextBox();
             label2 = new Label();
             label3 = new Label();
+            yCoordinateTextBox = new TextBox();
             label4 = new Label();
             SideLengthTextBox = new TextBox();
-            colorDialog1 = new ColorDialog();
+            button1 = new Button();
             button2 = new Button();
             button3 = new Button();
-            colorDialog2 = new ColorDialog();
-            trackBar1 = new TrackBar();
+            button5 = new Button();
+            button6 = new Button();
             button4 = new Button();
+            pictureBox1 = new PictureBox();
+            colorDialog1 = new ColorDialog();
+            colorDialog2 = new ColorDialog();
+            panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // panel1
             // 
-            button1.Font = new Font("Segoe UI", 9F);
-            button1.Location = new Point(12, 157);
-            button1.Name = "button1";
-            button1.RightToLeft = RightToLeft.No;
-            button1.Size = new Size(235, 44);
-            button1.TabIndex = 0;
-            button1.Text = "Намалювати трикутник";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Location = new Point(253, 12);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(1037, 589);
-            pictureBox1.TabIndex = 1;
-            pictureBox1.TabStop = false;
+            panel1.BackColor = Color.FromArgb(245, 245, 245);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(xCoordinateTextBox);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(yCoordinateTextBox);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(SideLengthTextBox);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button6);
+            panel1.Controls.Add(button4);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Padding = new Padding(15);
+            panel1.Size = new Size(260, 903);
+            panel1.TabIndex = 0;
             // 
             // label1
             // 
-            label1.Font = new Font("Segoe UI Semibold", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            label1.Location = new Point(12, 12);
+            label1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            label1.Location = new Point(15, 15);
             label1.Name = "label1";
-            label1.Size = new Size(235, 41);
-            label1.TabIndex = 2;
-            label1.Text = "Координати центру рівностороннього трикутника";
+            label1.Size = new Size(230, 45);
+            label1.TabIndex = 0;
+            label1.Text = "Центр трикутника (X, Y):";
             // 
             // xCoordinateTextBox
             // 
-            xCoordinateTextBox.Location = new Point(46, 79);
+            xCoordinateTextBox.Location = new Point(45, 60);
             xCoordinateTextBox.Name = "xCoordinateTextBox";
-            xCoordinateTextBox.Size = new Size(56, 27);
-            xCoordinateTextBox.TabIndex = 3;
-            // 
-            // yCoordinateTextBox
-            // 
-            yCoordinateTextBox.Location = new Point(152, 79);
-            yCoordinateTextBox.Name = "yCoordinateTextBox";
-            yCoordinateTextBox.Size = new Size(56, 27);
-            yCoordinateTextBox.TabIndex = 4;
+            xCoordinateTextBox.Size = new Size(70, 27);
+            xCoordinateTextBox.TabIndex = 2;
             // 
             // label2
             // 
-            label2.Font = new Font("Segoe UI", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            label2.Location = new Point(20, 83);
+            label2.Location = new Point(15, 63);
             label2.Name = "label2";
-            label2.Size = new Size(20, 19);
-            label2.TabIndex = 5;
-            label2.Text = "X: ";
+            label2.Size = new Size(25, 27);
+            label2.TabIndex = 1;
+            label2.Text = "X:";
             // 
             // label3
             // 
-            label3.Font = new Font("Segoe UI", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            label3.Location = new Point(126, 83);
+            label3.Location = new Point(135, 63);
             label3.Name = "label3";
-            label3.Size = new Size(20, 19);
-            label3.TabIndex = 6;
-            label3.Text = "Y: ";
+            label3.Size = new Size(25, 27);
+            label3.TabIndex = 3;
+            label3.Text = "Y:";
+            // 
+            // yCoordinateTextBox
+            // 
+            yCoordinateTextBox.Location = new Point(165, 60);
+            yCoordinateTextBox.Name = "yCoordinateTextBox";
+            yCoordinateTextBox.Size = new Size(70, 27);
+            yCoordinateTextBox.TabIndex = 4;
             // 
             // label4
             // 
-            label4.Font = new Font("Segoe UI Semibold", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            label4.Location = new Point(12, 118);
+            label4.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            label4.Location = new Point(15, 110);
             label4.Name = "label4";
-            label4.Size = new Size(121, 24);
-            label4.TabIndex = 7;
-            label4.Text = "Довжина сторони ";
+            label4.Size = new Size(230, 25);
+            label4.TabIndex = 5;
+            label4.Text = "Довжина сторони:";
             // 
             // SideLengthTextBox
             // 
-            SideLengthTextBox.Location = new Point(139, 115);
+            SideLengthTextBox.Location = new Point(15, 138);
             SideLengthTextBox.Name = "SideLengthTextBox";
-            SideLengthTextBox.Size = new Size(108, 27);
-            SideLengthTextBox.TabIndex = 8;
+            SideLengthTextBox.Size = new Size(220, 27);
+            SideLengthTextBox.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(220, 235, 252);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(15, 185);
+            button1.Name = "button1";
+            button1.Size = new Size(220, 45);
+            button1.TabIndex = 7;
+            button1.Text = "Намалювати";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
-            button2.Font = new Font("Segoe UI", 9F);
-            button2.Location = new Point(12, 207);
+            button2.Location = new Point(15, 236);
             button2.Name = "button2";
-            button2.RightToLeft = RightToLeft.No;
-            button2.Size = new Size(235, 44);
-            button2.TabIndex = 9;
-            button2.Text = "Змінити колір трикутника";
-            button2.UseVisualStyleBackColor = true;
+            button2.Size = new Size(220, 40);
+            button2.TabIndex = 8;
+            button2.Text = "Колір трикутника";
             button2.Click += button2_Click;
             // 
             // button3
             // 
-            button3.Font = new Font("Segoe UI", 9F);
-            button3.Location = new Point(12, 257);
+            button3.Location = new Point(15, 282);
             button3.Name = "button3";
-            button3.RightToLeft = RightToLeft.No;
-            button3.Size = new Size(235, 44);
-            button3.TabIndex = 10;
-            button3.Text = "Змінити колір медіан";
-            button3.UseVisualStyleBackColor = true;
+            button3.Size = new Size(220, 40);
+            button3.TabIndex = 9;
+            button3.Text = "Колір медіан";
             button3.Click += button3_Click;
             // 
-            // trackBar1
+            // button5
             // 
-            trackBar1.Location = new Point(1057, 607);
-            trackBar1.Maximum = 100;
-            trackBar1.Name = "trackBar1";
-            trackBar1.Size = new Size(233, 56);
-            trackBar1.TabIndex = 11;
-            trackBar1.Scroll += trackBar1_Scroll;
+            button5.Location = new Point(15, 350);
+            button5.Name = "button5";
+            button5.Size = new Size(220, 40);
+            button5.TabIndex = 10;
+            button5.Text = "Зберегти у файл";
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(15, 396);
+            button6.Name = "button6";
+            button6.Size = new Size(220, 40);
+            button6.TabIndex = 11;
+            button6.Text = "Завантажити";
+            button6.Click += button6_Click;
             // 
             // button4
             // 
-            button4.Font = new Font("Segoe UI", 9F);
-            button4.Location = new Point(12, 557);
+            button4.ForeColor = Color.DarkRed;
+            button4.Location = new Point(15, 460);
             button4.Name = "button4";
-            button4.RightToLeft = RightToLeft.No;
-            button4.Size = new Size(235, 44);
+            button4.Size = new Size(220, 40);
             button4.TabIndex = 12;
-            button4.Text = "Очистити все";
-            button4.UseVisualStyleBackColor = true;
+            button4.Text = "Очистити ";
             button4.Click += button4_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Location = new Point(260, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1522, 903);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1352, 673);
-            Controls.Add(button4);
-            Controls.Add(trackBar1);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(SideLengthTextBox);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(yCoordinateTextBox);
-            Controls.Add(xCoordinateTextBox);
-            Controls.Add(label1);
+            ClientSize = new Size(1782, 903);
             Controls.Add(pictureBox1);
-            Controls.Add(button1);
-            Font = new Font("Segoe UI", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 204);
+            Controls.Add(panel1);
+            Font = new Font("Segoe UI", 9F);
             Name = "Form1";
-            Text = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "лаб1 кг";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
+        private Panel panel1;
         private Button button1;
         private PictureBox pictureBox1;
         private Label label1;
@@ -211,7 +217,8 @@
         private Button button2;
         private Button button3;
         private ColorDialog colorDialog2;
-        private TrackBar trackBar1;
         private Button button4;
+        private Button button5;
+        private Button button6;
     }
 }
--- KG_LAB1/Form1.cs
+++ KG_LAB1/Form1.cs
@@ -2,12 +2,14 @@ using System.Drawing.Design;
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 using static System.Windows.Forms.AxHost;
+using System.IO;
 namespace KG_LAB1
 {
     public partial class Form1 : Form
     {
         public struct TriangleData
         {
+            
             public int X;
             public int Y;
             public int lenght;
@@ -22,9 +24,9 @@ namespace KG_LAB1
         Graphics g;
         public Form1()
         {
-            image = new Bitmap(1000, 720,
-                System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+
             InitializeComponent();
+            image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             g = Graphics.FromImage(image);
             g.Clear(Color.White);
             drawAxes();
@@ -33,29 +35,128 @@ namespace KG_LAB1
         }
         private void drawAxes()
         {
-            Pen pen = new Pen(Color.LightGray, 1);
-            g.DrawLine(pen, 0, pictureBox1.Height / 2, pictureBox1.Width, pictureBox1.Height / 2);
-            g.DrawLine(pen, pictureBox1.Width / 2, 0, pictureBox1.Width / 2, pictureBox1.Height);
+            if (g == null) return;
+
+            Pen gridPen = new Pen(Color.FromArgb(235, 235, 235), 1);
+            Pen axisPen = new Pen(Color.Black, 2);
+            Font font = new Font("Arial", 8);
+            Brush textBrush = Brushes.Black;
+
+            float centerX = pictureBox1.Width / 2f;
+            float centerY = pictureBox1.Height / 2f;
+
+            int step;
+            if (scale < 0.2) step = 200;
+            else if (scale < 0.7) step = 100;
+            else if (scale < 2.5) step = 50;
+            else if (scale < 6) step = 10;
+            else step = 1;
+
+            for (int i = -5000; i <= 5000; i += step)
+            {
+                float screenX = centerX + (i * scale);
+                if (screenX >= 0 && screenX <= pictureBox1.Width)
+                {
+                    g.DrawLine(gridPen, screenX, 0, screenX, pictureBox1.Height);
+                }
+            }
+
+            for (int i = -5000; i <= 5000; i += step)
+            {
+                float screenY = centerY - (i * scale);
+                if (screenY >= 0 && screenY <= pictureBox1.Height)
+                {
+                    g.DrawLine(gridPen, 0, screenY, pictureBox1.Width, screenY);
+                }
+            }
+
+            g.DrawLine(axisPen, 0, centerY, pictureBox1.Width, centerY);
+            g.DrawLine(axisPen, centerX, 0, centerX, pictureBox1.Height);
+
+            for (int i = -5000; i <= 5000; i += step)
+            {
+                float screenX = centerX + (i * scale);
+                float screenY = centerY - (i * scale);
+
+                if (screenX >= 0 && screenX <= pictureBox1.Width)
+                {
+                    g.DrawLine(axisPen, screenX, centerY - 5, screenX, centerY + 5);
+                    if (i != 0)
+                        g.DrawString(i.ToString(), font, textBrush, screenX - 10, centerY + 8);
+                }
+
+                if (screenY >= 0 && screenY <= pictureBox1.Height)
+                {
+                    g.DrawLine(axisPen, centerX - 5, screenY, centerX + 5, screenY);
+                    if (i != 0)
+                        g.DrawString(i.ToString(), font, textBrush, centerX + 8, screenY - 7);
+                }
+            }
+
         }
+
         private void button1_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(xCoordinateTextBox.Text) ||
+                string.IsNullOrWhiteSpace(yCoordinateTextBox.Text) ||
+                string.IsNullOrWhiteSpace(SideLengthTextBox.Text))
+            {
+                MessageBox.Show("Будь ласка, заповніть усі поля введення.");
+                return;
+            }
 
             if (int.TryParse(xCoordinateTextBox.Text, out int x0) && int.TryParse(yCoordinateTextBox.Text, out int y0) && int.TryParse(SideLengthTextBox.Text, out int lenght))
             {
-                trianglesList.Add(new TriangleData
+                if (lenght <= 0)
                 {
-                    X = x0,
-                    Y = y0,
-                    lenght = lenght,
-                    fillColor = triangleColor,
-                    medianColor = mediansColor
-                });
-                redrawAll();
+                    MessageBox.Show("Довжина сторони повинна бути додатною.");
+                    return;
+                }
+                else if (x0 < -5000 || x0 > 5000 || y0 < -5000 || y0 > 5000)
+                {
+                    MessageBox.Show("Координати повинні бути в діапазоні від -5000 до 5000.");
+                }
+                else
+                {
+                    trianglesList.Add(new TriangleData
+                    {
+                        X = x0,
+                        Y = y0,
+                        lenght = lenght,
+                        fillColor = triangleColor,
+                        medianColor = mediansColor
+                    });
+                    updateScale();
+                    redrawAll();
+                }
             }
+        }
+        private void updateScale()
+        {
+            if (trianglesList.Count == 0) return;
+
+            float maxCoord = 0;
+            foreach (var tri in trianglesList)
+            {
+                float currentMax = Math.Max(Math.Abs(tri.X) + tri.lenght, Math.Abs(tri.Y) + tri.lenght);
+                if (currentMax > maxCoord) maxCoord = currentMax;
+            }
+
+            float availableSpace = Math.Min(pictureBox1.Width, pictureBox1.Height) / 2.2f;
+            scale = availableSpace / maxCoord;
         }
         public void redrawAll()
         {
+            if (image == null || image.Width != pictureBox1.Width || image.Height != pictureBox1.Height)
+            {
+                image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                g = Graphics.FromImage(image);
+                pictureBox1.Image = image;
+            }
             g.Clear(Color.White);
+
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
             drawAxes();
 
             foreach (var tri in trianglesList)
@@ -76,12 +177,13 @@ namespace KG_LAB1
                 float R = (float)(sLenght / Math.Sqrt(3));
                 var state = g.Save();
                 g.TranslateTransform(pictureBox1.Width / 2, pictureBox1.Height / 2);
+                g.ScaleTransform(1, -1);
 
-                points[0] = new PointF(sx0, (int)(sy0 - R));
-                points[1] = new PointF(sx0 - sLenght / 2, (int)(sy0 + R / 2));
-                points[2] = new PointF(sx0 + sLenght / 2, (int)(sy0 + R / 2));
+                points[0] = new PointF(sx0, (int)(sy0 + R));
+                points[1] = new PointF(sx0 - sLenght / 2, (int)(sy0 - R / 2));
+                points[2] = new PointF(sx0 + sLenght / 2, (int)(sy0 - R / 2));
 
-                medians[0] = new PointF(sx0, (int)(sy0 + R / 2));
+                medians[0] = new PointF(sx0, (int)(sy0 - R / 2));
                 medians[1] = new PointF((int)((points[0].X + points[2].X) / 2f), (int)((points[0].Y + points[2].Y) / 2f));
                 medians[2] = new PointF((int)((points[0].X + points[1].X) / 2f), (int)((points[0].Y + points[1].Y) / 2f));
 
@@ -130,17 +232,80 @@ namespace KG_LAB1
             }
         }
 
-        private void trackBar1_Scroll(object sender, EventArgs e)
-        {
-            scale = trackBar1.Value / 10f;
-            redrawAll();
 
-        }
 
         private void button4_Click(object sender, EventArgs e)
         {
             trianglesList.Clear();
+            updateScale();
             redrawAll();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (trianglesList.Count == 0)
+            {
+                MessageBox.Show("Немає трикутників для збереження.");
+                return;
+            } else { 
+            List<string> lines = new List<string>();
+            foreach (var tri in trianglesList)
+            {
+                string line = $"{tri.X};{tri.Y};{tri.lenght};{tri.fillColor.ToArgb()};{tri.medianColor.ToArgb()}";
+                lines.Add(line);
+            }
+            File.WriteAllLines("triangles.txt", lines);
+            MessageBox.Show("Трикутники збережені у файл triangles.txt");
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if(File.Exists("triangles.txt"))
+            {
+                try
+                {
+                    trianglesList.Clear();
+
+                    string[] lines = File.ReadAllLines("triangles.txt");
+
+                    foreach (string line in lines)
+                    {
+                        string[] parts = line.Split(';');
+
+                        if (parts.Length == 5)
+                        {
+                            TriangleData tri = new TriangleData
+                            {
+                                X = int.Parse(parts[0]),
+                                Y = int.Parse(parts[1]),
+                                lenght = int.Parse(parts[2]),
+                                fillColor = Color.FromArgb(int.Parse(parts[3])),
+                                medianColor = Color.FromArgb(int.Parse(parts[4]))
+                            };
+                            trianglesList.Add(tri);
+                        }
+                    }
+
+                    updateScale();
+                    redrawAll();
+
+                    MessageBox.Show("Дані успішно завантажено з файлу!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Помилка при читанні файлу: {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Файл triangles.txt не знайдено.");
+            }
         }
     }
 }
